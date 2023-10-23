01e9eb99d4019b889e841a6eba414540ef94c7f3
edubel26 <beltran502qhotmail.com>
2023-10-23 18:23:46 -0500

    Actualizacion de la tabla Eps

--- Data/Seeds/SeedEps.sql
+++ Data/Seeds/SeedEps.sql
@@ -6,10 +6,37 @@ BEGIN
     IF(@check = 0)
     BEGIN
         INSERT INTO [dbo].[Eps] ([EpsName])
-        VALUES      ('Capital Salud'),
-                    ('Salud total'),
-                    ('ConfaNorte'),
-                    ('Sura'),
-                    ('Sanitas')
+        VALUES      ('aliansalud entidad promotora de salud s.a.'),
+                    ('anaswayuu'),
+                    ('asociación indígena del cauca'),
+                    ('asociacion mutual ser empresa solidaria de salud eps'),
+                    ('compensar e.p.s.'),
+                    ('cooperativa de salud y desarrollo integral zona sur oriental de cartagena'),
+                    ('e.p.s. famisanar ltda.'),
+                    ('e.p.s. sanitas s.a.'),
+                    ('eps servicio occidental de salud s.a.'),
+                    ('eps y medicina prepagada suramericana s.a'),
+                    ('nueva eps s.a.'),
+                    ('salud total s.a.  e.p.s.'),
+                    ('saludvida s.a. e.p.s'),
+                    ('savia salud eps'),
+                    ('capital salud'),
+                    ('e.p.s. famisanar ltda.'),
+                    ('e.p.s. sanitas  s.a.'),
+                    ('eps servicio occidental de salud  s.a.'),
+                    ('mallamas'),
+                    ('salud total s.a. e.p.s.'),
+                    ('capresoca eps'),
+                    ('comfenalco valle  e.p.s.'),
+                    ('eps convida'),
+                    ('saludvida s.a. e.p.s.'),
+                    ('pijaos salud epsi'),
+                    ('saludvida s.a .e.p.s'),
+                    ('asociación indígena del cesar y la guajira dusakawi'),
+                    ('eps s.o.s. s.a.'),
+                    ('fundación salud mia')
+
+                    
+
     END
 END
